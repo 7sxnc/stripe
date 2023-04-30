020077f7c810c6f956567122ac43a51e7b7ccd31
7sxnc <128174034+7sxnc@users.noreply.github.com>
2023-04-30 17:12:00 -0300

    Update Events.php

--- Events.php
+++ Events.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace humhub\modules\patreon;
+namespace humhub\modules\Stripe;
 
 use Yii;
 use yii\helpers\Url;
-use humhub\modules\patreon\widgets\PatreonFrame;
+use humhub\modules\Stripe\widgets\StripeFrame;
 use humhub\models\Setting;
 
 class Events extends \yii\base\BaseObject
@@ -13,23 +13,23 @@ class Events extends \yii\base\BaseObject
     public static function onAdminMenuInit(\yii\base\Event $event)
     {
         $event->sender->addItem([
-            'label' => Yii::t('PatreonModule.base', 'Patreon Settings'),
-            'url' => Url::toRoute('/patreon/admin/index'),
+            'label' => Yii::t('StripeModule.base', 'Stripe Settings'),
+            'url' => Url::toRoute('/Stripe/admin/index'),
             'group' => 'settings',
-            'icon' => '<i class="fab fa-patreon"></i>',
-            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'patreon' && Yii::$app->controller->id == 'admin',
+            'icon' => '<i class="fab fa-Stripe"></i>',
+            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'Stripe' && Yii::$app->controller->id == 'admin',
             'sortOrder' => 650
         ]);
     }
 
-public static function addPatreonFrame($event)
+public static function addStripeFrame($event)
     {
         if (Yii::$app->user->isGuest) {
             return;
         }
         $event->sender->view->registerAssetBundle(Assets::class);
-        $event->sender->addWidget(PatreonFrame::class, [], [
-            'sortOrder' => Setting::Get('timeout', 'patreon')
+        $event->sender->addWidget(StripeFrame::class, [], [
+            'sortOrder' => Setting::Get('timeout', 'Stripe')
         ]);
     }
 }
