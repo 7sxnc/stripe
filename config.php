7688a87a51c21c4a1f274d0621eee1aa4e90ab02
7sxnc <128174034+7sxnc@users.noreply.github.com>
2023-04-30 17:06:28 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,33 +1,33 @@
 <?php
 
-namespace humhub\modules\patreon;
+namespace humhub\modules\stripe;
 
 return [
-    'id' => 'patreon',
-    'class' => 'humhub\modules\patreon\Module',
-    'namespace' => 'humhub\modules\patreon',
+    'id' => 'stripe',
+    'class' => 'humhub\modules\stripe\Module',
+    'namespace' => 'humhub\modules\stripe',
     'events' => [
         [
             'class' => \humhub\modules\dashboard\widgets\Sidebar::class,
             'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
             'callback' => [
-                'humhub\modules\patreon\Events',
-                'addPatreonFrame'
+                'humhub\modules\Stripe\Events',
+                'addStripeFrame'
             ]
         ],
         [
             'class' => \humhub\modules\space\widgets\Sidebar::class,
             'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
             'callback' => [
-                'humhub\modules\patreon\Events',
-                'addPatreonFrame'
+                'humhub\modules\Stripe\Events',
+                'addStripeFrame'
             ]
         ],
         [
             'class' => \humhub\modules\admin\widgets\AdminMenu::class,
             'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
             'callback' => [
-                'humhub\modules\patreon\Events',
+                'humhub\modules\Stripe\Events',
                 'onAdminMenuInit'
             ]
         ]
